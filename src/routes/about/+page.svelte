956a21398bdf7abcb34bcb3974e66af2b82eb0b3
Eric BREHAULT <ebrehault@gmail.com>
2023-03-12 17:05:16 +0100

    fix About page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,7 +1,20 @@
-<script lang="ts">
-	import { speechCommand } from '$lib/execute';
-</script>
-
 <h1>About</h1>
 
-<button use:speechCommand={'about'}>Say "About"</button>
+<h2>Where is the code?</h2>
+<p>
+	Here is the <a href="https://github.com/ebrehault/talk2svelte">Talk2Svelte library repository</a>.
+	It also contains this demo app full code.
+</p>
+<p>Add a star on the GitHub repository if you like it!</p>
+
+<h2>Browser support</h2>
+<p>
+	The Web Speech API is not supported by all browsers (for example, it is not supported by Firefox
+	or Edge), have a look to <a href="https://caniuse.com/speech-recognition"
+		>the current support status</a
+	>.
+</p>
+<p>
+	Also, on non-Chrome Chromium based browsers, the speech recognition might not work as expected
+	(for example on Brave).
+</p>
